0e1c9659552b04dd7c2aad2af0dfc9f80fc2a4d7
pulsar <pulsar-de@users.noreply.github.com>
2015-12-16 03:02:03 +0100

    cmd_mass.lua: possibility to send mass without sender

--- scripts/cmd_mass.lua
+++ scripts/cmd_mass.lua
@@ -2,35 +2,43 @@
 
     cmd_mass.lua by blastbeat
 
-        - this script adds a command "mass" to send a pm mass message
-        - usage: [+!#]mass <MSG>  /  [+!#]masslvl <LEVEL> <MSG>
-        
+        - this script adds commands to send pm mass messages
+        - usage: [+!#]mass <MSG> / [+!#]masslvl <LEVEL> <MSG> / [+!#]masshub <MSG>
+
+        v0.16: by pulsar
+            - improved dateparser()
+            - renamed "msg_out_op" to "msg_out_lvl"
+            - possibility to send mass without sender  / requested by Sopor
+                - added onbmsg_hub()
+                - added cmd_hub
+            - some code improvements
+
         v0.15: by pulsar
             - removed "cmd_mass_minlevel" import
                 - using util.getlowestlevel( tbl ) instead of "cmd_mass_minlevel"
-                
+
         v0.14: by pulsar
             - changed date style in output messages to: yyyy-mm-dd
             - code cleaning
-        
+
         v0.13: by pulsar
             - changed visual output style
-        
+
         v0.12: by pulsar
             - send mass to specific levels for ops
             - code cleaning
             - table lookups
-            
+
         v0.11: by pulsar
             - changed visual output style
             - table lookups
 
         v0.10: by pulsar
             - changed rightclick style
-        
+
         v0.09: by pulsar
             - export scriptsettings to "/cfg/cfg.tbl"
-        
+
         v0.08: by blastbeat
             - updated script api
             - regged hubcommand
@@ -56,10 +64,11 @@
 --------------
 
 local scriptname = "cmd_mass"
-local scriptversion = "0.15"
+local scriptversion = "0.16"
 
 local cmd = "mass"
 local cmd_lvl = "masslvl"
+local cmd_hub = "masshub"
 
 
 ----------------------------
@@ -76,34 +85,28 @@ local hub_import = hub.import
 local hub_debug = hub.debug
 local utf_match = utf.match
 local utf_format = utf.format
-local os_date = os.date
 local util_getlowestlevel = util.getlowestlevel
+local os_date = os.date
+local table_sort = table.sort
 
 --// imports
-local help, ucmd, hubcmd
 local oplevel = cfg_get( "cmd_mass_oplevel" )
 local permission = cfg_get( "cmd_mass_permission" )
 local levels = cfg_get( "levels" )
 local scriptlang = cfg_get( "language" )
-
---// functions
-local dateparser
-local onbmsg
-local onbmsg_op
-
---// msgs
-
 local lang, err = cfg_loadlanguage( scriptlang, scriptname ); lang = lang or {}; err = err and hub_debug( err )
 
+--// msgs
 local help_title = lang.help_title or "mass"
-local help_usage = lang.help_usage or "[+!#]mass <message>"
-local help_desc = lang.help_desc or "sends a pm with <message> to all users"
+local help_usage = lang.help_usage or "[+!#]mass <MSG>"
+local help_desc = lang.help_desc or "sends a pm with <MSG> to all users"
 
 local help_title_op = lang.help_title_op or "masslvl"
-local help_usage_op = lang.help_usage_op or "[+!#]masslvl <level> <message>"
-local help_desc_op = lang.help_desc_op or "sends a pm with <message> to all users with specific level"
+local help_usage_op = lang.help_usage_op or "[+!#]masslvl <LEVEL> <MSG> / [+!#]masshub <MSG>"
+local help_desc_op = lang.help_desc_op or "sends a pm with <MSG> to all users with specific level / sends a pm with <MSG> without sender"
 
 local ucmd_menu = lang.ucmd_menu or { "User", "Messages", "Mass", "to all" }
+local ucmd_menu_hub = lang.ucmd_menu_hub or { "User", "Messages", "Mass", "to all (without sender)" }
 local ucmd_menu_1 = lang.ucmd_menu_1 or "User"
 local ucmd_menu_2 = lang.ucmd_menu_2 or "Messages"
 local ucmd_menu_3 = lang.ucmd_menu_3 or "Mass"
@@ -111,8 +114,8 @@ local ucmd_menu_4 = lang.ucmd_menu_4 or "to level"
 local ucmd_what = lang.ucmd_what or "Message:"
 
 local msg_denied = lang.msg_denied or "You are not allowed to use this command."
-local msg_usage = lang.msg_usage or "Usage: [+!#]mass <message>"
-local msg_usage_op = lang.msg_usage_op or "Usage: [+!#]masslvl <level> <message>"
+local msg_usage = lang.msg_usage or "Usage: [+!#]mass <MSG>"
+local msg_usage_op = lang.msg_usage_op or "Usage: [+!#]masslvl <LEVEL> <MSG>"
 local msg_lvl_exists = lang.msg_lvl_exists or "Level %s does not exists."
 local msg_ok = lang.msg_ok or "Mass was send to all users with level: "
 local msg_out = lang.msg_out or [[
@@ -126,8 +129,8 @@ Message:  %s
 
 ====================================================================================================== MASS MESSAGE ===
   ]]
-  
-local msg_out_op = lang.msg_out_op or [[
+
+local msg_out_lvl = lang.msg_out_lvl or [[
 
 
 === MASS MESSAGE ======================================================================================================
@@ -139,6 +142,24 @@ Message:  %s
 ====================================================================================================== MASS MESSAGE ===
   ]]
 
+local msg_out_hub = lang.msg_out_hub or [[
+
+
+=== MASS MESSAGE ======================================================================================================
+
+Date:  %s   |   Time:  %s
+
+Message:  %s
+
+====================================================================================================== MASS MESSAGE ===
+  ]]
+
+--// functions
+local dateparser
+local onbmsg
+local onbmsg_lvl
+local onbmsg_hub
+
 
 ----------
 --[CODE]--
@@ -147,54 +168,7 @@ Message:  %s
 local minlevel = util_getlowestlevel( permission )
 
 dateparser = function()
-    if scriptlang == "de" then
-        local wochentage = {
-            ["Monday"] = "Montag",
-            ["Tuesday"] = "Dienstag",
-            ["Wednesday"] = "Mittwoch",
-            ["Thursday"] = "Donnerstag",
-            ["Friday"] = "Freitag",
-            ["Saturday"] = "Samstag",
-            ["Sunday"] = "Sonntag",
-        }
-        local monate = {
-            ["January"] = "Januar",
-            ["February"] = "Februar",
-            ["March"] = "März",
-            ["April"] = "April",
-            ["May"] = "Mai",
-            ["June"] = "Juni",
-            ["July"] = "Juli",
-            ["August"] = "August",
-            ["September"] = "September",
-            ["October"] = "Oktober",
-            ["November"] = "November",
-            ["December"] = "Dezember",
-        }
-        local day = os_date( "%d" )
-        local month = os_date( "%B" )
-        local year = os_date( "%Y" )
-        local weekday = os_date( "%A" )
-        local time = os_date( "%X" )
-        local wochentag = wochentage[ weekday ]
-        local monat = monate[ month ]
-        local datum = wochentag .. ", der " .. day .. "." .. monat .. "." .. year
-        return datum, time
-    elseif scriptlang == "en" then
-        local day = os_date( "%d" )
-        local month = os_date( "%m" )
-        local year = os_date( "%Y" )
-        local time = os_date( "%X" )
-        local date = year .. "-" .. month .. "-" .. day
-        return date, time
-    else
-        local day = os_date( "%d" )
-        local month = os_date( "%m" )
-        local year = os_date( "%Y" )
-        local time = os_date( "%X" )
-        local date = year .. "-" .. month .. "-" .. day
-        return date, time
-    end
+    return os_date( "%Y" ) .. "-" .. os_date( "%m" ) .. "-" .. os_date( "%d" ), os_date( "%X" )
 end
 
 onbmsg = function( user, command, msg )
@@ -215,7 +189,24 @@ onbmsg = function( user, command, msg )
     return PROCESSED
 end
 
-onbmsg_op = function( user, command, msg )
+onbmsg_hub = function( user, command, msg )
+    local msg = utf_match( msg, "(.+)" )
+    if not permission[ user:level() ] then
+        user:reply( msg_denied, hub_getbot )
+        return PROCESSED
+    end
+    if not msg then
+        user:reply( msg_usage, hub_getbot )
+        return PROCESSED
+    else
+        local date, time = dateparser()
+        local mass = utf_format( msg_out_hub, date, time, msg )
+        hub_broadcast( mass, user, hub_getbot )
+    end
+    return PROCESSED
+end
+
+onbmsg_lvl = function( user, command, msg )
     local lvl, msg = utf_match( msg, "(%d+) (.+)" )
     local lvl = tonumber( lvl )
     if user:level() < oplevel then
@@ -238,7 +229,7 @@ onbmsg_op = function( user, command, msg )
         local nick = user:nick()
         local date, time = dateparser()
         local levelname = cfg_get( "levels" )[ lvl ] or "UNREG"
-        local mass = utf_format( msg_out_op, nick, date, time, lvl .. " [ " .. levelname .. " ]", msg )
+        local mass = utf_format( msg_out_lvl, nick, date, time, lvl .. " [ " .. levelname .. " ]", msg )
         for sid, target in pairs( hub_getusers() ) do
             if not target:isbot() then
                 if target:level() == lvl then
@@ -248,19 +239,20 @@ onbmsg_op = function( user, command, msg )
         end
         user:reply( msg_ok .. lvl .. " [ " .. levelname .. " ]", hub_getbot )
         return PROCESSED
-    end  
+    end
 end
 
 hub.setlistener( "onStart", { },
     function( )
-        help = hub_import( "cmd_help" )
+        local help = hub_import( "cmd_help" )
         if help then
             help.reg( help_title, help_usage, help_desc, minlevel )
             help.reg( help_title_op, help_usage_op, help_desc_op, oplevel )
         end
-        ucmd = hub_import( "etc_usercommands" )
+        local ucmd = hub_import( "etc_usercommands" )
         if ucmd then
             ucmd.add( ucmd_menu, cmd, { "%[line:" .. ucmd_what .. "]" }, { "CT1" }, minlevel )
+            ucmd.add( ucmd_menu_hub, cmd_hub, { "%[line:" .. ucmd_what .. "]" }, { "CT1" }, oplevel )
             local tbl = {}
             local i = 1
             for k, v in pairs( levels ) do
@@ -269,15 +261,16 @@ hub.setlistener( "onStart", { },
                     i = i + 1
                 end
             end
-            table.sort( tbl )
+            table_sort( tbl )
             for _, level in pairs( tbl ) do
                 ucmd.add( { ucmd_menu_1, ucmd_menu_2, ucmd_menu_3, ucmd_menu_4, levels[ level ] }, cmd_lvl, { level, "%[line:" .. ucmd_what .. "]" }, { "CT1" }, oplevel )
             end
         end
-        hubcmd = hub_import( "etc_hubcommands" )
+        local hubcmd = hub_import( "etc_hubcommands" )
         assert( hubcmd )
         assert( hubcmd.add( cmd, onbmsg ) )
-        assert( hubcmd.add( cmd_lvl, onbmsg_op ) )
+        assert( hubcmd.add( cmd_lvl, onbmsg_lvl ) )
+        assert( hubcmd.add( cmd_hub, onbmsg_hub ) )
         return nil
     end
 )
