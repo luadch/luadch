5ac316191e610d996d0904b5f6f78e622faeb057
pulsar <pulsar-de@users.noreply.github.com>
2015-12-19 15:36:58 +0100

    etc_hubcommands.lua: add support for multiple commands, usage: hubcmd.add( { cmd1, cmd2, cmd3 ... }, onbmsg )

--- scripts/etc_hubcommands.lua
+++ scripts/etc_hubcommands.lua
@@ -2,7 +2,11 @@
 
         etc_hubcommands.lua v0.01 by blastbeat
 
-        - this script exports a module to reg hubcommands
+        v0.02: by pulsar
+            - add support for multiple commands, usage: hubcmd.add( { cmd1, cmd2, cmd3 ... }, onbmsg )
+
+        v0.01: by blastbeat
+            - this script exports a module to reg hubcommands
 
 ]]--
 
@@ -14,17 +18,27 @@ local scriptname = "etc_hubcommands"
 local scriptversion = "0.01"
 
 local utf_match = utf.match
-
 local hub_getbot = hub.getbot
 
 local commands = { }
 
 local add = function( cmd, func )    -- quick and dirty...
-    if commands[ cmd ] then
-        return false
+    if type( cmd ) == "table" then
+        local state = false
+        for k, v in pairs( cmd ) do
+            if not commands[ v ] then
+                commands[ v ] = func
+                state = true
+            end
+        end
+        if state then return true else return false end
     else
-        commands[ cmd ] = func
-        return true
+        if commands[ cmd ] then
+            return false
+        else
+            commands[ cmd ] = func
+            return true
+        end
     end
 end
 
