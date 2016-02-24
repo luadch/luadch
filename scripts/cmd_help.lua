714ebe0ee78099c6365429d68cf642cd2d378075
pulsar <pulsar-de@users.noreply.github.com>
2016-02-24 16:09:14 +0100

    cmd_help.lua: small typo fix; some small code changes; add table lookups

--- scripts/cmd_help.lua
+++ scripts/cmd_help.lua
@@ -6,58 +6,69 @@
         - it exports also a module to reg a help text which will be shown by help
         - usage: [+!#]help
 
-        - v0.05: by pulsar
+        v0.06: by pulsar
+            - small typo fix
+            - some small code changes
+            - add table lookups
+
+        v0.05: by pulsar
             - changed visual output style
-            
-        - v0.04: by blastbeat
-          - updated script api
-          - regged hubcommand
 
-        - v0.03: by blastbeat
-          - some clean ups
+        v0.04: by blastbeat
+            - updated script api
+            - regged hubcommand
 
-        - v0.02: by blastbeat
-          - added language files and ucmd
+        v0.03: by blastbeat
+            - some clean ups
+
+        v0.02: by blastbeat
+            - added language files and ucmd
 
 ]]--
 
---// settings begin //--
 
 local scriptname = "cmd_help"
-local scriptversion = "0.05"
-local scriptlang = cfg.get( "language" )
+local scriptversion = "0.06"
 
 local cmd = "help"
 
---// settings end //--
+local minlevel = 0    -- minimum level to get the help
 
+--// table lookups
+local cfg_get = cfg.get
+local cfg_loadlanguage = cfg.loadlanguage
+local hub_import = hub.import
+local hub_debug = hub.debug
 local hub_getbot = hub.getbot
+local hub_debug = hub.debug
 local utf_match = utf.match
 local utf_format = utf.format
+local table_concat = table.concat
 
-local help = {}
-local lang, err = cfg.loadlanguage( scriptlang, scriptname ); lang = lang or {}; err = err and hub.debug( err )
+--// imports
+local scriptlang = cfg_get( "language" )
+local lang, err = cfg_loadlanguage( scriptlang, scriptname ); lang = lang or {}; err = err and hub_debug( err )
 
-local msg_usage = lang.msg_usage or "Usage: "
-local msg_description = lang.msg_description or "Description: "
-local msg_minlevel = lang.msg_minlevel or "Level: "
+--// msgs
+local help_title = lang.help_title or "cmd_help.lua"
+local help_usage = lang.help_usage or "[+!#]help"
+local help_desc = lang.help_desc or "Shows this help for hub commands"
+
+local msg_usage = lang.msg_usage or "Usage:"
+local msg_description = lang.msg_description or "Description:"
+local msg_minlevel = lang.msg_minlevel or "Min. Level:"
 local msg_out = lang.msg_out or [[
 
 
 === AVAILABLE COMMANDS =================================================================================
 %s
-
-
 ================================================================================= AVAILABLE COMMANDS ===
   ]]
 
-local help_title = lang.help_title or "help"
-local help_usage = lang.help_usage or "[+!#]help"
-local help_desc = lang.help_desc or "shows help for hub commands"
+local ucmd_menu = lang.ucmd_menu or { "General", "Help" }
 
-local help_level = 0    -- minimum level to get the help
-
-local ucmd_menu = lang.ucmd_menu or { "Help" }
+--// code
+local help = {}
 
 local reghelp = function( title, usage, desc, level )
     title, usage, desc = tostring( title ), tostring( usage ), tostring( desc )
@@ -65,17 +76,19 @@ local reghelp = function( title, usage, desc, level )
     help[ #help + 1 ] = { title = title, usage = usage, desc = desc, level = level }
 end
 
-local hubcmd
-
 local onbmsg = function( user, command, parameters )
     local tmp = {}
     local level = user:level()
     for id, tbl in ipairs( help ) do
         if level >= tbl.level then
-            tmp[ #tmp + 1 ] = "\n\n" .. tbl.title .. "\n" .. msg_usage .. "\t\t" .. tbl.usage .. "\n" .. msg_description .. "\t" .. tbl.desc .. "\n" .. msg_minlevel .. "\t\t" .. tbl.level
+            tmp[ #tmp + 1 ] = "\n" ..
+                              tbl.title .. "\n" ..
+                              msg_usage .. "\t\t" .. tbl.usage .. "\n" ..
+                              msg_description .. "\t" .. tbl.desc .. "\n" ..
+                              msg_minlevel .. "\t" .. tbl.level .. "\n"
         end
     end
-    tmp = table.concat( tmp )
+    tmp = table_concat( tmp )
     local msg = utf_format( msg_out, tmp )
     user:reply( msg, hub_getbot(), hub_getbot() )
     return PROCESSED
@@ -83,20 +96,20 @@ end
 
 hub.setlistener( "onStart", { },
     function( )
-        local ucmd = hub.import "etc_usercommands"    -- add usercommand
+        local ucmd = hub_import( "etc_usercommands" )    -- add usercommand
         if ucmd then
-            ucmd.add( ucmd_menu, cmd, { }, { "CT1" }, help_level )
+            ucmd.add( ucmd_menu, cmd, { }, { "CT1" }, minlevel )
         end
-        hubcmd = hub.import "etc_hubcommands"    -- add hubcommand
+        local hubcmd = hub_import( "etc_hubcommands" )    -- add hubcommand
         assert( hubcmd )
         assert( hubcmd.add( cmd, onbmsg ) )
         return nil
     end
 )
 
-reghelp( help_title, help_usage, help_desc, help_level )
+reghelp( help_title, help_usage, help_desc, minlevel )
 
-hub.debug( "** Loaded "..scriptname.." "..scriptversion.." **" )
+hub_debug( "** Loaded "..scriptname.." "..scriptversion.." **" )
 
 --// public //--
 
