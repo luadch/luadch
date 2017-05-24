0105556c81c7a54dd6dd3a79c366b6711e8929e0
blastbeat <lastbeat@nowhere>
2017-05-24 17:44:48 +0200

    Change version to 2.19

--- core/const.lua
+++ core/const.lua
@@ -11,8 +11,8 @@
 return {
 
     PROGRAM_NAME = "Luadch",
-    VERSION = "v2.19 RC",
-    COPYRIGHT = "by blastbeat and pulsar (2007-2016)",
+    VERSION = "v2.19",
+    COPYRIGHT = "by blastbeat and pulsar (2007-2017)",
     CONFIG_PATH = "././cfg/",
     -- LOG_PATH = "././log/",
     -- CORE_PATH = "././core/",
