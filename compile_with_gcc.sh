e3beb167d0cdb9d69f61dd182d87d019f7cd9ba8
blasti <sqor@arcor.de>
2017-07-15 14:50:21 +0200

    Create proper makefiles for Linux

--- compile_with_gcc.sh
+++ compile_with_gcc.sh
@@ -27,34 +27,28 @@ rsync -av --exclude=".svn" ${ROOT}/examples/certs ${INSTALL_DIR}/
 rsync -av --exclude=".svn" ${ROOT}/examples/lang ${INSTALL_DIR}/
 rsync -av --exclude=".svn" ${ROOT}/docs ${INSTALL_DIR}/
 
-cd ${ROOT}/lua/src
-
+echo
 echo Building lua...
-gcc -O3 -Wall -DLUA_USE_POSIX -DLUA_USE_DLOPEN -fpic -c *.c
-gcc -shared -fpic -o liblua.so lapi.o lcode.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o lmem.o lobject.o lopcodes.o lparser.o lstate.o lstring.o ltable.o ltm.o lundump.o lvm.o lzio.o lauxlib.o lbaselib.o ldblib.o liolib.o lmathlib.o loslib.o ltablib.o lstrlib.o loadlib.o linit.o
-cp liblua.so ${INSTALL_DIR}/
+echo
+cd ${ROOT}/lua/src
+make INSTALL_DIR=$INSTALL_DIR
 
-cd ${ROOT}/adclib 
-
+echo
 echo Building adclib...
-g++ -O3 -Wall -fpic -c -I$LUA_DIR *.cpp
-g++ -shared -fpic -o adclib.so *.o -L$LUA_DIR -llua
-cp adclib.so $INSTALL_DIR/lib/adclib/adclib.so
-rm *.o
-rm *.so
+echo
+cd ${ROOT}/adclib 
+make LUAINCLUDE_DIR=$LUA_DIR LUALIB_DIR=$INSTALL_DIR INSTALL_DIR=$INSTALL_DIR/lib/adclib/adclib.so
 
-cd ${ROOT}/slnunicode
-
+echo
 echo Building unicode...
-gcc -O3 -Wall -fpic -c -I$LUA_DIR *.c
-gcc -shared -fpic -o unicode.so *.o -L$LUA_DIR -llua
-cp unicode.so $INSTALL_DIR/lib/unicode/unicode.so
-rm *.o
-rm *.so
+echo
+cd ${ROOT}/slnunicode
+make LUAINCLUDE_DIR=$LUA_DIR LUALIB_DIR=$INSTALL_DIR INSTALL_DIR=$INSTALL_DIR/lib/unicode/unicode.so
 
-cd ${ROOT}/luasocket/src
-
+echo
 echo Building luasocket...
+echo
+cd ${ROOT}/luasocket/src
 mv mime.c mime.c.not
 mv wsocket.c wsocket.c.not
 gcc -O3 -c -Wall -fpic -I$LUA_DIR *.c
@@ -89,10 +83,6 @@ gcc -o luadch *.o -L$LUA_DIR -L/opt/lib, -llua -lm -ldl -Wl,-rpath .
 cp luadch $INSTALL_DIR/luadch
 rm luadch
 rm *.o
-
-cd ${ROOT}/lua/src
-rm *.o
-rm *.so
 
 cd ${INSTALL_DIR}/
 
