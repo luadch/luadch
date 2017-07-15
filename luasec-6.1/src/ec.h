eeec00ad8fb11bf1f800a179b224a82996c612ea
blasti <sqor@arcor.de>
2017-07-15 13:43:32 +0200

    Delete Submodule

---
+++ luasec-6.1/src/ec.h
@@ -0,0 +1,21 @@
+/*--------------------------------------------------------------------------
+ * LuaSec 0.6
+ * Copyright (C) 2006-2016 Bruno Silvestre
+ *
+ *--------------------------------------------------------------------------*/
+
+#ifndef LSEC_EC_H
+#define LSEC_EC_H
+
+#include <lua.h>
+
+#ifndef OPENSSL_NO_ECDH
+#include <openssl/ec.h>
+
+EC_KEY *lsec_find_ec_key(lua_State *L, const char *str);
+#endif
+
+void lsec_get_curves(lua_State *L);
+void lsec_load_curves(lua_State *L);
+
+#endif
