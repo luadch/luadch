5e64e35f9c33193ef03eae4f85411fcf63642202
blasti <blasti@users.noreply.github.com>
2020-04-25 13:18:30 +0200

    Fix #6; remove release mode (-r)

--- hub/hub.c
+++ hub/hub.c
@@ -7,16 +7,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef __unix__
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+
+#endif
+
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
 
-int pass = 0;
-int debug = 1;
+static int pass_count = 0;
+static int do_daemonization = 0;
 
-void help(void);
-void execute(void);
-void onerror(const char *msg);
+static void daemonize(void);
+static void help(void);
+static void execute(void);
+static void onerror(const char *msg);
+static void handle_args(int argc, char **argv);
+
 static int restart(lua_State *L);
 static int cleantable(lua_State *L);
 static int tablesize(lua_State *L);
@@ -32,9 +47,11 @@ static int tablesize(lua_State *L) {
   return 1;
 }
 
-static int cleantable(lua_State *L) {
+static int cleantable(lua_State *L)
+{
   lua_pushnil(L);
-  while (lua_next(L, 1) != 0) {
+  while (lua_next(L, 1) != 0)
+  {
     lua_pop(L, 1);
     lua_pushnil(L);
     lua_rawset(L, 1);
@@ -43,14 +60,16 @@ static int cleantable(lua_State *L) {
   return 0;
 }
 
-static int restart(lua_State *L) {
+static int restart(lua_State *L)
+{
   lua_close(L);
   atexit(execute);
   exit(EXIT_SUCCESS);
   return EXIT_SUCCESS;
 }
 
-void onerror(const char *msg) {
+void onerror(const char *msg)
+{
   FILE *file;
   file = fopen("exception.txt", "a+");
   if (NULL != file) {
@@ -61,51 +80,79 @@ void onerror(const char *msg) {
   fflush(stderr);
 }
 
-void execute(void) {
+void daemonize(void)
+{
+#ifdef __unix__
+
+  pid_t pid = fork();
+  if (pid < 0) exit(EXIT_FAILURE);
+  if (pid > 0) exit(EXIT_SUCCESS);
+  umask(0);
+  if (setsid() < 0) exit(EXIT_FAILURE);
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+#else
+
+  fprintf(stderr, "Daemonization is not implemented on your OS yet.");
+
+#endif
+}
+
+void execute(void)
+{
   int error;
   lua_State *L = lua_open();
-  if (NULL == L) {
+  if (NULL == L)
+  {
     onerror("cannot create state: not enough memory");
     exit(EXIT_FAILURE);
   }
   luaL_openlibs(L);
-  if (debug == 0 && pass == 0) {
-    pass = 1;
-    lua_pushboolean(L, 0);
-    lua_setglobal(L, "DEBUG");
-  }
-  else if (pass == 0) {
+  if (pass_count == 0)
+  {
     lua_pushboolean(L, 1);
     lua_setglobal(L, "DEBUG");
   }
+  if (do_daemonization) daemonize();
   lua_register(L, "restartluadch", restart);
   lua_register(L, "cleantable", cleantable);
   lua_register(L, "tablesize", tablesize);
   error = luaL_loadfile(L, "core/init.lua") || lua_pcall(L, 0, 0, 0);
-  if (error)
-    onerror(lua_tostring(L, -1));
+  if (error) onerror(lua_tostring(L, -1));
   lua_close(L);
   exit(EXIT_SUCCESS);
 }
 
-void help(void) {
+void help(void)
+{
   fprintf(stderr,
-  "usage: luadch [options]\n"
+  "usage: luadch [option]\n"
   "available options are:\n"
-  "  -r       execute luadch in release mode (no debug messages from hub to screen and event.log)\n");
+  "  -h       show this help\n"
+  "  -d       execute luadch as background daemon\n");
   fflush(stderr);
   exit(EXIT_SUCCESS);
 }
 
-int main(int argc, char **argv) {
-  if (argc > 1 && argv[1][0] == '-') {
-    switch(argv[1][1]) {
+void handle_args(int argc, char **argv)
+{
+  if (argc > 1 && argv[1][0] == '-')
+  {
+    switch(argv[1][1])
+    {
       case 'h': help();
         break;
-      case 'r': debug = 0;
+      case 'd': do_daemonization = 1;
         break;
     }
   }
+}
+
+int main(int argc, char **argv)
+{
+  handle_args(argc, argv);
   execute();
   return EXIT_SUCCESS;
 }
