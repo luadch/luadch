2864e58dc7ccef2f1076e0170906f7f417379e9c
pulsar-de <pulsar@pulsnet.org>
2021-09-02 00:40:37 +0200

    fix #53

--- scripts/cmd_usersearch.lua
+++ scripts/cmd_usersearch.lua
@@ -4,6 +4,11 @@
 
         usage: [+!#]usersearch <searchstring>
 
+        v1.3: by pulsar
+            - removed table lookups
+            - fix #53
+                - show userlist in alphabetical order sorted by firstnick
+
         v1.2: by HypoManiac
             - Only shows nick for users with same or higher level.
 
@@ -52,47 +57,23 @@
 --------------
 
 local scriptname = "cmd_usersearch"
-local scriptversion = "1.2"
+local scriptversion = "1.3"
 
 local cmd = "usersearch"
 
 -- Should details be hidden for users of same or higher level
 local hide_details_for_same_or_higher = false
 
-
-----------------------------
---[DEFINITION/DECLARATION]--
-----------------------------
-
---// table lookups
-local cfg_get = cfg.get
-local cfg_loadlanguage = cfg.loadlanguage
-local hub_debug = hub.debug
-local hub_getbot = hub.getbot()
-local hub_getusers = hub.getusers
-local hub_getregusers = hub.getregusers
-local hub_isnickonline = hub.isnickonline
-local hub_import = hub.import
-local hub_escapeto = hub.escapeto
-local utf_format = utf.format
-local string_find = string.find
-local table_insert = table.insert
-local util_formatseconds = util.formatseconds
-local os_difftime = os.difftime
-local os_time = os.time
-local util_date = util.date
-local util_difftime = util.difftime
-
 --// imports
 local help, ucmd, hubcmd
-local prefix_table = cfg_get( "usr_nick_prefix_prefix_table" )
-local activate = cfg_get( "usr_nick_prefix_activate" )
-local minlevel = cfg_get( "cmd_usersearch_minlevel" )
-local max_limit = cfg_get( "cmd_usersearch_max_limit" )
-local scriptlang = cfg_get( "language" )
+local prefix_table = cfg.get( "usr_nick_prefix_prefix_table" )
+local activate = cfg.get( "usr_nick_prefix_activate" )
+local minlevel = cfg.get( "cmd_usersearch_minlevel" )
+local max_limit = cfg.get( "cmd_usersearch_max_limit" )
+local scriptlang = cfg.get( "language" )
 
 --// msgs
-local lang, err = cfg_loadlanguage( scriptlang, scriptname ); lang = lang or { }; err = err and hub_debug( err )
+local lang, err = cfg.loadlanguage( scriptlang, scriptname ); lang = lang or { }; err = err and hub.debug( err )
 
 local help_title = lang.help_title or "usersearch"
 local help_usage = lang.help_usage or "[+!#]usersearch <searchstring>"
@@ -126,33 +107,18 @@ local get_lastlogout = function( profile )
     local lastlogout
     local ll = profile.lastlogout or profile.lastconnect
     local ll_str = tostring( ll )
-    --[[
-    if profile.is_online == 1 then
-        lastlogout = msg_online
-    elseif ll then
-        if #ll_str == 14 then
-            local sec, y, d, h, m, s = util_difftime( util_date(), ll )
-            lastlogout = y .. msg_years .. d .. msg_days .. h .. msg_hours .. m .. msg_minutes .. s .. msg_seconds
-        else
-            local d, h, m, s = util_formatseconds( os_difftime( os_time(), ll ) )
-            lastlogout = d .. msg_days .. h .. msg_hours .. m .. msg_minutes .. s .. msg_seconds
-        end
-    else
-        lastlogout = msg_unknown
-    end
-    ]]
     local found = false
-    for sid, user in pairs( hub_getusers() ) do
+    for sid, user in pairs( hub.getusers() ) do
         if user:firstnick() == profile.nick then found = true break end
     end
     if found then
         lastlogout = msg_online
     elseif ll then
         if #ll_str == 14 then
-            local sec, y, d, h, m, s = util_difftime( util_date(), ll )
+            local sec, y, d, h, m, s = util.difftime( util.date(), ll )
             lastlogout = y .. msg_years .. d .. msg_days .. h .. msg_hours .. m .. msg_minutes .. s .. msg_seconds
         else
-            local d, h, m, s = util_formatseconds( os_difftime( os_time(), ll ) )
+            local d, h, m, s = util.formatseconds( os.difftime( os.time(), ll ) )
             lastlogout = d .. msg_days .. h .. msg_hours .. m .. msg_minutes .. s .. msg_seconds
         end
     else
@@ -161,10 +127,29 @@ local get_lastlogout = function( profile )
     return lastlogout
 end
 
+--// like ipairs for alphabetical indices
+local spairs = function( tbl, func )
+    local arr = {}
+    for n in pairs( tbl ) do
+        table.insert( arr, n )
+    end
+    table.sort( arr, func )
+    local i = 0
+    local iter = function()
+        i = i + 1
+        if arr[ i ] == nil then
+            return nil
+        else
+            return arr[ i ], tbl[ arr[ i ] ]
+        end
+    end
+    return iter
+end
+
 local onbmsg = function( user, command, parameters )
     local user_level = user:level( )
     if user_level < minlevel then
-        user:reply( msg_denied, hub_getbot )
+        user:reply( msg_denied, hub.getbot() )
         return PROCESSED
     end
     parameters = parameters:gsub( "[%%^$().[%]*+?-]", "%%%0" )  -- escape magic chars
@@ -175,24 +160,23 @@ local onbmsg = function( user, command, parameters )
     local ret = {}
     local count = 0
     local max_limit_reached = false
-    local regusers, _, __ = hub_getregusers( )
+    local regusers, _, __ = hub.getregusers( )
     for i, u in ipairs( regusers ) do
-        local found = show_all or string_find( u.nick:lower(), parameters:lower() )
+        local found = show_all or string.find( u.nick:lower(), parameters:lower() )
         if found and not u.is_bot then
             if count <= max_limit then
                 count = count + 1
                 if u.level >= user_level and hide_details_for_same_or_higher then
-                    table_insert( ret, utf_format( msg_result_nick, u.nick ) )
+                    ret[ u.nick ] = utf.format( msg_result_nick, u.nick )
                 else
-                    table_insert( ret, utf_format(
+                    ret[ u.nick ] = utf.format(
                         msg_result,
                         u.nick,
                         u.level or msg_unknown,
                         ( ( user_level == 100 ) or ( user_level > ( u.level or 0 ) ) ) and ( u.password or msg_unknown ) or msg_no_allowed,
                         u.by or msg_unknown,
                         u.date or msg_unknown,
-                        get_lastlogout( u )
-                    ))
+                        get_lastlogout( u ) )
                 end
             else
                 max_limit_reached = true
@@ -201,36 +185,36 @@ local onbmsg = function( user, command, parameters )
     end
     local msg = "\n"
     local hasres = false
-    for i, b in ipairs( ret ) do
+    for k, v in spairs( ret ) do
         hasres = true
-        msg = msg .. b .. "\n"
+        msg = msg .. v .. "\n"
     end
     if hasres then
         if max_limit_reached then
             msg = msg .. msg_max_limit
         end
-        user:reply( msg, hub_getbot )
+        user:reply( msg, hub.getbot() )
         return PROCESSED
     end
-    user:reply( msg_no_matches, hub_getbot )
+    user:reply( msg_no_matches, hub.getbot() )
     return PROCESSED
 end
 
 hub.setlistener( "onStart", {},
     function( )
-        help = hub_import( "cmd_help" )
+        help = hub.import( "cmd_help" )
         if help then
             help.reg( help_title, help_usage, help_desc, minlevel )
         end
-        ucmd = hub_import( "etc_usercommands" )    -- add usercommand
+        ucmd = hub.import( "etc_usercommands" )    -- add usercommand
         if ucmd then
             ucmd.add( ucmd_menu, cmd, { "%[line:" .. ucmd_popup .. "]" }, { "CT1" }, minlevel )
         end
-        hubcmd = hub_import( "etc_hubcommands" )
+        hubcmd = hub.import( "etc_hubcommands" )
         assert( hubcmd )
         assert( hubcmd.add( cmd, onbmsg ) )
         return nil
     end
 )
 
-hub_debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " **" )
+hub.debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " **" )
