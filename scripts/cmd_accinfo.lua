aee5c6d895c3286721c9743e099d57eef8c71cdc
pulsar-de <pulsar@pulsnet.org>
2021-09-02 03:28:11 +0200

    fix #31

--- scripts/cmd_accinfo.lua
+++ scripts/cmd_accinfo.lua
@@ -5,6 +5,12 @@
         - this script adds a command "accinfo" get infos about a reguser
         - usage: [+!#]accinfo sid|nick <SID>|<NICK> / [+!#]accinfoop sid|nick <SID>|<NICK>
 
+        v0.23: by pulsar
+            - removed table lookups
+            - shows expanded accinfo as default (op level)
+            - fix #31 / thx Sopor
+                - shows if user is banned or not
+
         v0.22: by pulsar
             - fix #107 / thx Sopor
                 - shows if the user is blocked by trafficmanager/msgmanager
@@ -94,52 +100,28 @@
 --------------
 
 local scriptname = "cmd_accinfo"
-local scriptversion = "0.22"
+local scriptversion = "0.23"
 
 local cmd = "accinfo"
 local cmd2 = "accinfoop"
 
-
-----------------------------
---[DEFINITION/DECLARATION]--
-----------------------------
-
---// table lookups
-local cfg_get = cfg.get
-local cfg_loadlanguage = cfg.loadlanguage
-local utf_match = utf.match
-local hub_getbot = hub.getbot
-local hub_getusers = hub.getusers
-local hub_getregusers = hub.getregusers
-local hub_escapeto = hub.escapeto
-local hub_isnickonline = hub.isnickonline
-local hub_debug = hub.debug
-local hub_import = hub.import
-local utf_format = utf.format
-local util_date = util.date
-local util_difftime = util.difftime
-local util_getlowestlevel = util.getlowestlevel
-local util_loadtable = util.loadtable
-local util_formatseconds = util.formatseconds
-local os_time = os.time
-local os_difftime = os.difftime
-local table_concat = table.concat
-
 --// imports
 local help, ucmd, hubcmd
-local scriptlang = cfg_get( "language" )
-local lang, err = cfg_loadlanguage( scriptlang, scriptname ); lang = lang or { }; err = err and hub_debug( err )
-local permission = cfg_get( "cmd_accinfo_permission" )
-local tcp = cfg_get( "tcp_ports" )
-local ssl = cfg_get( "ssl_ports" )
-local host = cfg_get( "hub_hostaddress" )
-local hname = cfg_get( "hub_name" )
-local use_keyprint = cfg_get( "use_keyprint" )
-local keyprint_type = cfg_get( "keyprint_type" )
-local keyprint_hash = cfg_get( "keyprint_hash" )
-local advanced_rc = cfg_get( "cmd_accinfo_advanced_rc" )
-local msgmanager_activate = cfg_get( "etc_msgmanager_activate" )
-local trafficmanager_activate = cfg_get( "etc_trafficmanager_activate" )
+local scriptlang = cfg.get( "language" )
+local lang, err = cfg.loadlanguage( scriptlang, scriptname ); lang = lang or { }; err = err and hub.debug( err )
+local permission = cfg.get( "cmd_accinfo_permission" )
+local tcp = cfg.get( "tcp_ports" )
+local ssl = cfg.get( "ssl_ports" )
+local host = cfg.get( "hub_hostaddress" )
+local hname = cfg.get( "hub_name" )
+local use_keyprint = cfg.get( "use_keyprint" )
+local keyprint_type = cfg.get( "keyprint_type" )
+local keyprint_hash = cfg.get( "keyprint_hash" )
+local advanced_rc = cfg.get( "cmd_accinfo_advanced_rc" )
+local msgmanager_activate = cfg.get( "etc_msgmanager_activate" )
+local trafficmanager_activate = cfg.get( "etc_trafficmanager_activate" )
+local ban = hub.import( "cmd_ban")
+local bans_tbl = ban.bans
 
 --// msgs
 local help_title = lang.help_title or "cmd_accinfo.lua - Users"
@@ -178,6 +160,7 @@ local msg_accinfo = lang.msg_accinfo or [[
 
     Traffic blocked: %s
     Messages blocked: %s
+    Nickname is banned: %s
 
     Hubname: %s
     Hubaddress: %s
@@ -220,20 +203,24 @@ local ucmd_menu_ct4 = lang.ucmd_menu_ct4 or "User"
 local ucmd_menu_ct5 = lang.ucmd_menu_ct5 or "Accinfo"
 local ucmd_menu_ct6 = lang.ucmd_menu_ct6 or "by Nick from List"
 
-local ucmd_menu_ct1_op = lang.ucmd_menu_ct1_op or { "User", "Accinfo", "expanded", "by Nick" }
+local ucmd_menu_ct1_op = lang.ucmd_menu_ct1_op or { "User", "Accinfo" }
 --local ucmd_menu_ct2_op = lang.ucmd_menu_ct2_op or { "User", "Accinfo", "expanded", "by CID" }
-local ucmd_menu_ct3_op = lang.ucmd_menu_ct3_op or { "Show", "Accinfo", "expanded" }
+local ucmd_menu_ct3_op = lang.ucmd_menu_ct3_op or { "Show", "Accinfo" }
 local ucmd_menu_ct4_op = lang.ucmd_menu_ct4_op or "User"
-local ucmd_menu_ct5_op = lang.ucmd_menu_ct5_op or "Accinfo (expanded)"
+local ucmd_menu_ct5_op = lang.ucmd_menu_ct5_op or "Accinfo"
 local ucmd_menu_ct6_op = lang.ucmd_menu_ct6_op or "by Nick from List"
 
-local msg_msgmanager = "%s %s"
-local msg_msgmanager_1 = "YES / Blockmode: "
-local msg_msgmanager_2 = "NO"
+local msg_msgmanager = lang.msg_msgmanager or "%s %s"
+local msg_msgmanager_1 = lang.msg_msgmanager_1 or "YES / Blockmode: "
+local msg_msgmanager_2 = lang.msg_msgmanager_2 or "NO"
 
-local msg_trafficmanager_1 = "YES"
-local msg_trafficmanager_2 = "NO"
-local search_flag_blocked = "[BLOCKED]"
+local msg_trafficmanager_1 = lang.msg_trafficmanager_1 or "YES"
+local msg_trafficmanager_2 = lang.msg_trafficmanager_2 or "NO"
+local search_flag_blocked = lang.search_flag_blocked or "[BLOCKED]"
+
+local msg_bans_yes = lang.msg_bans_yes or "YES / banned by: %s / bantime remaining: %s"
+local msg_bans_no = lang.msg_bans_no or "NO"
+local msg_forever = lang.msg_forever or "forever"
 
 --// database
 local description_file = "scripts/data/cmd_reg_descriptions.tbl"
@@ -243,31 +230,31 @@ local msgmanager_file = "scripts/data/etc_msgmanager.tbl"
 --[CODE]--
 ----------
 
-local oplevel = util_getlowestlevel( permission )
+local oplevel = util.getlowestlevel( permission )
 local description_tbl, msgmanager_tbl
 local addy = ""
 
 if #tcp ~= 0 then
     if #tcp > 1 then
-        --addy = addy .. "adc://" .. host .. ":" .. table_concat( tcp, ", " ) .. "    "
+        --addy = addy .. "adc://" .. host .. ":" .. table.concat( tcp, ", " ) .. "    "
         addy = addy .. "\n"
         for i, port in ipairs( tcp ) do
             addy = addy .. "\t\tadc://" .. host .. ":" .. port .. "\n"
         end
     else
-        addy = addy .. "adc://" .. host .. ":" .. table_concat( tcp, ", " ) .. "    "
+        addy = addy .. "adc://" .. host .. ":" .. table.concat( tcp, ", " ) .. "    "
     end
 end
 if #ssl ~= 0 then
     if #ssl > 1 then
         if use_keyprint then
-            --addy = addy .. "adcs://" .. host .. ":" .. table_concat( ssl, ", " ) .. keyprint_type .. keyprint_hash
+            --addy = addy .. "adcs://" .. host .. ":" .. table.concat( ssl, ", " ) .. keyprint_type .. keyprint_hash
             addy = addy .. "\n"
             for i, port in ipairs( ssl ) do
                 addy = addy .. "\n\t\tadcs://" .. host .. ":" .. port .. keyprint_type .. keyprint_hash
             end
         else
-            --addy = addy .. "adcs://" .. host .. ":" .. table_concat( ssl, ", " )
+            --addy = addy .. "adcs://" .. host .. ":" .. table.concat( ssl, ", " )
             addy = addy .. "\n"
             for i, port in ipairs( ssl ) do
                 addy = addy .. "\n\t\tadcs://" .. host .. ":" .. port
@@ -275,9 +262,9 @@ if #ssl ~= 0 then
         end
     else
         if use_keyprint then
-            addy = addy .. "adcs://" .. host .. ":" .. table_concat( ssl, ", " ) .. keyprint_type .. keyprint_hash
+            addy = addy .. "adcs://" .. host .. ":" .. table.concat( ssl, ", " ) .. keyprint_type .. keyprint_hash
         else
-            addy = addy .. "adcs://" .. host .. ":" .. table_concat( ssl, ", " )
+            addy = addy .. "adcs://" .. host .. ":" .. table.concat( ssl, ", " )
         end
     end
 end
@@ -291,10 +278,10 @@ local get_lastlogout = function( profile )
         lastlogout = msg_online
     elseif ll then
         if #ll_str == 14 then
-            local sec, y, d, h, m, s = util_difftime( util_date(), ll )
+            local sec, y, d, h, m, s = util.difftime( util.date(), ll )
             lastlogout = y .. msg_years .. d .. msg_days .. h .. msg_hours .. m .. msg_minutes .. s .. msg_seconds
         else
-            local d, h, m, s = util_formatseconds( os_difftime( os_time(), ll ) )
+            local d, h, m, s = util.formatseconds( os.difftime( os.time(), ll ) )
             lastlogout = d .. msg_days .. h .. msg_hours .. m .. msg_minutes .. s .. msg_seconds
         end
     else
@@ -302,17 +289,17 @@ local get_lastlogout = function( profile )
     end
     ]]
     local found = false
-    for sid, user in pairs( hub_getusers() ) do
+    for sid, user in pairs( hub.getusers() ) do
         if user:firstnick() == profile.nick then found = true break end
     end
     if found then
         lastlogout = msg_online
     elseif ll then
         if #ll_str == 14 then
-            local sec, y, d, h, m, s = util_difftime( util_date(), ll )
+            local sec, y, d, h, m, s = util.difftime( util.date(), ll )
             lastlogout = y .. msg_years .. d .. msg_days .. h .. msg_hours .. m .. msg_minutes .. s .. msg_seconds
         else
-            local d, h, m, s = util_formatseconds( os_difftime( os_time(), ll ) )
+            local d, h, m, s = util.formatseconds( os.difftime( os.time(), ll ) )
             lastlogout = d .. msg_days .. h .. msg_hours .. m .. msg_minutes .. s .. msg_seconds
         end
     else
@@ -322,7 +309,7 @@ local get_lastlogout = function( profile )
 end
 
 local get_regdescription = function( profile )
-    description_tbl = util_loadtable( description_file )
+    description_tbl = util.loadtable( description_file )
     local desc = ""
     for k, v in pairs( description_tbl ) do
         if k == profile.nick then
@@ -336,7 +323,7 @@ end
 local get_trafficmanager = function( profile )
     if trafficmanager_activate then
         local isBlocked = false
-        for sid, user in pairs( hub_getusers() ) do
+        for sid, user in pairs( hub.getusers() ) do
             if profile.nick == user:firstnick() then
                 local isBlocked, b = string.find( user:description(), search_flag_blocked, 1, true )
                 if isBlocked then return msg_trafficmanager_1 end
@@ -348,7 +335,7 @@ end
 
 local get_msgmanager = function( profile )
     if msgmanager_activate then
-        msgmanager_tbl = util_loadtable( msgmanager_file )
+        msgmanager_tbl = util.loadtable( msgmanager_file )
         local info = ""
         for k, v in pairs( msgmanager_tbl ) do
             if k == profile.nick then
@@ -356,30 +343,53 @@ local get_msgmanager = function( profile )
                 break
             end
         end
-        if info == "m" then return utf_format( msg_msgmanager, msg_msgmanager_1, "Main" ) end
-        if info == "p" then return utf_format( msg_msgmanager, msg_msgmanager_1, "PM" ) end
-        if info == "b" then return utf_format( msg_msgmanager, msg_msgmanager_1, "Main + PM" ) end
+        if info == "m" then return utf.format( msg_msgmanager, msg_msgmanager_1, "Main" ) end
+        if info == "p" then return utf.format( msg_msgmanager, msg_msgmanager_1, "PM" ) end
+        if info == "b" then return utf.format( msg_msgmanager, msg_msgmanager_1, "Main + PM" ) end
     end
     return msg_msgmanager_2
+end
+
+local is_banned = function( username )
+    local by_nick, start, time, reason
+    for k, v in pairs( bans_tbl ) do
+        if v.nick == username then
+            by_nick = v.by_nick
+            start = v.start
+            time = v.time
+            reason = v.reason
+            return by_nick, start, time, reason
+        end
+    end
+    return nil
+end
+
+local get_bantime = function( remaining )
+    if tostring( remaining ):find( "-" ) then
+        return msg_forever
+    else
+        local d, h, m, s = util.formatseconds( remaining )
+        return d .. msg_days .. h .. msg_hours .. m .. msg_minutes .. s .. msg_seconds
+    end
 end
 
 local onbmsg = function( user, command, parameters )
     local level = user:level()
     if level < 10 then
-        user:reply( msg_denied, hub_getbot() )
+        user:reply( msg_denied, hub.getbot() )
         return PROCESSED
     end
-    local me = utf_match( parameters, "^(%S+)" )
-    local by, id = utf_match( parameters, "^(%S+) (.*)" )
+    local me = utf.match( parameters, "^(%S+)" )
+    local by, id = utf.match( parameters, "^(%S+) (.*)" )
     local target
-    local _, regnicks, regcids = hub_getregusers()
-    local _, usersids = hub_getusers()
+    local _, regnicks, regcids = hub.getregusers()
+    local _, usersids = hub.getusers()
     if ( me == nil ) then
         local usercid, usernick = user:cid(), user:firstnick()
         target = regnicks[ usernick ] or regcids.TIGR[ usercid ]
     else
         if not ( ( by == "sid" or by == "nick" or by == "cid" ) and id ) then
-            user:reply( msg_usage, hub_getbot() )
+            user:reply( msg_usage, hub.getbot() )
             return PROCESSED
         else
             target = (
@@ -389,16 +399,16 @@ local onbmsg = function( user, command, parameters )
         end
     end
     if not target then
-        user:reply( msg_off, hub_getbot() )
+        user:reply( msg_off, hub.getbot() )
         return PROCESSED
     end
     local targetlevel = tonumber( target.level ) or 100
-    local targetlevelname = cfg_get( "levels" )[ targetlevel ] or "Unreg"
+    local targetlevelname = cfg.get( "levels" )[ targetlevel ] or "Unreg"
     if not ( me == nil ) and ( ( permission[ level ] or 0 ) < targetlevel ) then
-        user:reply( msg_god, hub_getbot() )
+        user:reply( msg_god, hub.getbot() )
         return PROCESSED
     end
-    local accinfo = utf_format(
+    local accinfo = utf.format(
         msg_accinfo2,
         target.nick or msg_unknown,
         targetlevel or msg_unknown,
@@ -406,35 +416,34 @@ local onbmsg = function( user, command, parameters )
         target.password or msg_unknown,
         target.by or msg_unknown,
         target.date or msg_unknown,
-        --get_regdescription( target ),
         get_lastlogout( target ),
         hname or msg_unknown,
         addy or msg_unknown
     )
-    user:reply( accinfo, hub_getbot(), hub_getbot() )
+    user:reply( accinfo, hub.getbot(), hub.getbot() )
     return PROCESSED
 end
 
 hub.setlistener( "onBroadcast", {},
     function( user, adccmd, parameters )
         local level = user:level()
-        local cmd, _ = utf_match( parameters, "^[+!#](%S+) (.+)" )
-        local me = utf_match( parameters, "^[+!#]%S+ (%S+)" )
-        local by, id = utf_match( parameters, "^[+!#]%S+ (%S+) (.*)" )
+        local cmd, _ = utf.match( parameters, "^[+!#](%S+) (.+)" )
+        local me = utf.match( parameters, "^[+!#]%S+ (%S+)" )
+        local by, id = utf.match( parameters, "^[+!#]%S+ (%S+) (.*)" )
         if cmd == cmd2 then
             if level < 10 then
-                user:reply( msg_denied, hub_getbot() )
+                user:reply( msg_denied, hub.getbot() )
                 return PROCESSED
             end
             local target
-            local _, regnicks, regcids = hub_getregusers()
-            local _, usersids = hub_getusers()
+            local _, regnicks, regcids = hub.getregusers()
+            local _, usersids = hub.getusers()
             if ( me == nil ) then
                 local usercid, usernick = user:cid(), user:firstnick()
                 target = regnicks[ usernick ] or regcids.TIGR[ usercid ]
             else
                 if not ( ( by == "sid" or by == "nick" or by == "cid" ) and id ) then
-                    user:reply( msg_usage, hub_getbot() )
+                    user:reply( msg_usage, hub.getbot() )
                     return PROCESSED
                 else
                     target = (
@@ -444,16 +453,23 @@ hub.setlistener( "onBroadcast", {},
                 end
             end
             if not target then
-                user:reply( msg_off, hub_getbot() )
+                user:reply( msg_off, hub.getbot() )
                 return PROCESSED
             end
             local targetlevel = tonumber( target.level ) or 100
-            local targetlevelname = cfg_get( "levels" )[ targetlevel ] or "Unreg"
+            local targetlevelname = cfg.get( "levels" )[ targetlevel ] or "Unreg"
             if not ( user.profile() == target ) and ( ( permission[ level ] or 0 ) < targetlevel ) then
-                user:reply( msg_god, hub_getbot() )
+                user:reply( msg_god, hub.getbot() )
                 return PROCESSED
             end
-            local accinfo = utf_format(
+
+            local ban_msg = msg_bans_no
+            local by_nick, start, time, reason = is_banned( target.nick )
+            if by_nick then
+                local remaining = time - os.difftime( os.time(), start )
+                ban_msg = utf.format( msg_bans_yes, by_nick, get_bantime( remaining ) )
+            end
+            local accinfo = utf.format(
                 msg_accinfo,
                 target.nick or msg_unknown,
                 targetlevel or msg_unknown,
@@ -465,10 +481,11 @@ hub.setlistener( "onBroadcast", {},
                 get_lastlogout( target ),
                 get_trafficmanager( target ),
                 get_msgmanager( target ),
+                ban_msg,
                 hname or msg_unknown,
                 addy or msg_unknown
             )
-            user:reply( accinfo, hub_getbot(), hub_getbot() )
+            user:reply( accinfo, hub.getbot(), hub.getbot() )
             return PROCESSED
         end
         return nil
@@ -477,24 +494,24 @@ hub.setlistener( "onBroadcast", {},
 
 hub.setlistener( "onStart", {},
     function()
-        help = hub_import( "cmd_help" )
+        help = hub.import( "cmd_help" )
         if help then
             help.reg( help_title, help_usage, help_desc, 10 )
             help.reg( help_title2, help_usage2, help_desc2, oplevel )
         end
-        ucmd = hub_import( "etc_usercommands" )    -- add usercommand
+        ucmd = hub.import( "etc_usercommands" )    -- add usercommand
         if ucmd then
             ucmd.add( ucmd_menu_ct0, cmd, { }, { "CT1" }, 10 )
-            ucmd.add( ucmd_menu_ct1, cmd, { "nick", "%[line:" .. ucmd_nick .. "]" }, { "CT1" }, oplevel )
+            --ucmd.add( ucmd_menu_ct1, cmd, { "nick", "%[line:" .. ucmd_nick .. "]" }, { "CT1" }, oplevel )
             --ucmd.add( ucmd_menu_ct2, cmd, { "cid", "%[line:" .. ucmd_cid .. "]" }, { "CT1" }, oplevel )
-            ucmd.add( ucmd_menu_ct3, cmd, { "sid", "%[userSID]" }, { "CT2" }, oplevel )
+            --ucmd.add( ucmd_menu_ct3, cmd, { "sid", "%[userSID]" }, { "CT2" }, oplevel )
 
             ucmd.add( ucmd_menu_ct1_op, cmd2, { "nick", "%[line:" .. ucmd_nick .. "]" }, { "CT1" }, oplevel )
             --ucmd.add( ucmd_menu_ct2_op, cmd2, { "cid", "%[line:" .. ucmd_cid .. "]" }, { "CT1" }, oplevel )
             ucmd.add( ucmd_menu_ct3_op, cmd2, { "sid", "%[userSID]" }, { "CT2" }, oplevel )
 
             if advanced_rc then
-                local regusers, reggednicks, reggedcids = hub_getregusers()
+                local regusers, reggednicks, reggedcids = hub.getregusers()
                 local usertbl = {}
                 for i, user in ipairs( regusers ) do
                     if ( user.is_bot ~=1 ) and user.nick then
@@ -508,11 +525,11 @@ hub.setlistener( "onStart", {},
                 end
             end
         end
-        hubcmd = hub_import( "etc_hubcommands" )    -- add hubcommand
+        hubcmd = hub.import( "etc_hubcommands" )    -- add hubcommand
         assert( hubcmd )
         assert( hubcmd.add( cmd, onbmsg ) )
         return nil
     end
 )
 
-hub_debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " **" )
+hub.debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " **" )
