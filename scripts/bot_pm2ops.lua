34ae062481fbbd4d03338c48bd7fb669a4fd8230
pulsar-de <pulsar@pulsnet.org>
2021-09-11 18:20:38 +0200

    removed table lookups

--- scripts/bot_pm2ops.lua
+++ scripts/bot_pm2ops.lua
@@ -2,19 +2,22 @@
 
     bot_pm2ops.lua by pulsar
 
-        v0.5 by blastbeat:
+        v0.6: by pulsar
+            - removed table lookups
+
+        v0.5: by blastbeat:
             - simplify 'activate' logic
 
         v0.4:
             - check if opchat is activated
-        
+
         v0.3:
             - removed "opchat_check"
-            
+
         v0.2:
             - small permission fix
             - check if opchat is active (for feeds)
-            
+
         v0.1:
             - this script regs a PmToOps Bot to send messages to OpChat
 
@@ -26,34 +29,23 @@
 --------------
 
 local scriptname = "bot_pm2ops"
-local scriptversion = "0.4"
+local scriptversion = "0.6"
 
 
 ----------------------------
 --[DEFINITION/DECLARATION]--
 ----------------------------
 
---// table lookups
-local cfg_get = cfg.get
-local cfg_loadlanguage = cfg.loadlanguage
-local hub_getbot = hub.getbot()
-local hub_getuser = hub.getuser
-local hub_getusers = hub.getusers
-local hub_import = hub.import
-local hub_debug = hub.debug
-local hub_escapefrom = hub.escapefrom
-local utf_format = utf.format
-
 --// imports
-local activate = cfg_get( "bot_pm2ops_activate" )
-local nick = cfg_get( "bot_pm2ops_nick" )
-local desc = cfg_get( "bot_pm2ops_desc" )
-local permission = cfg_get( "bot_pm2ops_permission" )
-local scriptlang = cfg_get( "language" )
-local opchat = hub_import( "bot_opchat" )
+local activate = cfg.get( "bot_pm2ops_activate" )
+local nick = cfg.get( "bot_pm2ops_nick" )
+local desc = cfg.get( "bot_pm2ops_desc" )
+local permission = cfg.get( "bot_pm2ops_permission" )
+local scriptlang = cfg.get( "language" )
+local opchat = hub.import( "bot_opchat" )
 
 --// msgs
-local lang, err = cfg_loadlanguage( scriptlang, scriptname ); lang = lang or {}; err = err and hub_debug( err )
+local lang, err = cfg.loadlanguage( scriptlang, scriptname ); lang = lang or {}; err = err and hub.debug( err )
 
 local msg_denied = lang.msg_denied or "You are not allowed to use this chat."
 local msg_send = lang.msg_send or "Done, your message was successfully send to all Operators."
@@ -65,17 +57,17 @@ local msg_toops = lang.msg_toops or "New  %s  message  |  From: %s  |  Msg: %s"
 ----------
 
 if (not activate) or (not opchat) then
-   hub_debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " (not active) **" )
+   hub.debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " (not active) **" )
    return
 end
 
 local client = function( bot, cmd )
     if cmd:fourcc() == "EMSG" then
-        local user = hub_getuser( cmd:mysid() )
+        local user = hub.getuser( cmd:mysid() )
         if not user then return true end
         local bot_nick, user_nick, user_level = bot:nick(), user:nick(), user:level()
-        local msg = hub_escapefrom( cmd:pos( 4 ) )
-        local msg_out = utf_format( msg_toops, bot_nick, user_nick, msg )
+        local msg = hub.escapefrom( cmd:pos( 4 ) )
+        local msg_out = utf.format( msg_toops, bot_nick, user_nick, msg )
         if not permission[ user_level ] then
             user:reply( msg_denied, bot, bot )
             return true
@@ -91,5 +83,4 @@ local pm2ops, err = hub.regbot{ nick = nick, desc = desc, client = client }
 
 err = err and error( err )
 
-hub_debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " **" )
-
+hub.debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " **" )
