22a72c460ff3685370460dd8de8b8a9ec031fd27
pulsar-de <pulsar@pulsnet.org>
2021-09-25 09:53:12 +0200

    removed unnecessary table entries

--- core/hci.lua
+++ core/hci.lua
@@ -4,11 +4,6 @@ hci_tbl = {
 
     [ "hubruntime" ] = 0,
     [ "hubruntime_last_check" ] = 0,
-    [ "hubshare" ] = 0,
-    [ "reload" ] = false,
-    [ "restart" ] = false,
-    [ "shutdown" ] = false,
-    [ "usercount" ] = 0,
 
 }
 
