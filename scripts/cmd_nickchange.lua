87ea747d885ad9b321070dbf080aad4f7fd973c8
pulsar-de <pulsar@pulsnet.org>
2021-10-15 04:05:14 +0200

    - update: "scripts/cmd_nickchange.lua"     - added "hub.updateusers()"         - fix #140 -> https://github.com/luadch/luadch/issues/140     - changed visuals     - removed table lookups     - changed: "scripts/lang/cmd_nickchange.lang.en"     - changed: "scripts/lang/cmd_nickchange.lang.de"

--- scripts/cmd_nickchange.lua
+++ scripts/cmd_nickchange.lua
@@ -8,6 +8,12 @@
 
         note: this script needs "nick_change = true" in "cfg/cfg.tbl"
 
+        v1.6:
+            - added "hub.updateusers()"
+                - fix #140 -> https://github.com/luadch/luadch/issues/140
+            - changed visuals
+            - removed table lookups
+
         v1.5:
             - fix #128
                 - detect unknown nicks
@@ -67,57 +73,33 @@
 --------------
 
 local scriptname = "cmd_nickchange"
-local scriptversion = "1.5"
+local scriptversion = "1.6"
 
 local cmd = "nickchange"
 local cmd_param_1 = "mynick"
 local cmd_param_2 = "othernick"
 local cmd_param_3 = "othernicku"
 
-
-----------------------------
---[DEFINITION/DECLARATION]--
-----------------------------
-
---// table lookups
-local cfg_get = cfg.get
-local cfg_loadlanguage = cfg.loadlanguage
-local utf_match = utf.match
-local utf_format = utf.format
-local hub_debug = hub.debug
-local hub_getbot = hub.getbot()
-local hub_getusers = hub.getusers
-local hub_getregusers = hub.getregusers
-local hub_escapeto = hub.escapeto
-local hub_import = hub.import
-local hub_debug = hub.debug
-local hub_isnickonline = hub.isnickonline
-local util_loadtable = util.loadtable
-local util_savearray = util.savearray
-local util_savetable = util.savetable
-local table_insert = table.insert
-local table_sort = table.sort
-local string_len = string.len
-
 --// imports
 local help, ucmd, hubcmd
-local scriptlang = cfg_get( "language" )
-local lang, err = cfg_loadlanguage( scriptlang, scriptname ); lang = lang or {}; err = err and hub_debug( err )
-local nick_change = cfg_get( "nick_change" )
-local min_length = cfg_get( "min_nickname_length" )
-local max_length = cfg_get( "max_nickname_length" )
-local minlevel = cfg_get( "cmd_nickchange_minlevel" )
-local oplevel = cfg_get( "cmd_nickchange_oplevel" )
-local activate = cfg_get( "usr_nick_prefix_activate" )
-local prefix_table = cfg_get( "usr_nick_prefix_prefix_table" )
-local advanced_rc = cfg_get( "cmd_nickchange_advanced_rc" )
-local report = hub_import( "etc_report" )
-local report_activate = cfg_get( "cmd_nickchange_report" )
-local report_hubbot = cfg_get( "cmd_nickchange_report_hubbot" )
-local report_opchat = cfg_get( "cmd_nickchange_report_opchat" )
+local scriptlang = cfg.get( "language" )
+local lang, err = cfg.loadlanguage( scriptlang, scriptname ); lang = lang or {}; err = err and hub.debug( err )
+local nick_change = cfg.get( "nick_change" )
+local min_length = cfg.get( "min_nickname_length" )
+local max_length = cfg.get( "max_nickname_length" )
+local minlevel = cfg.get( "cmd_nickchange_minlevel" )
+local oplevel = cfg.get( "cmd_nickchange_oplevel" )
+local activate = cfg.get( "usr_nick_prefix_activate" )
+local prefix_table = cfg.get( "usr_nick_prefix_prefix_table" )
+local advanced_rc = cfg.get( "cmd_nickchange_advanced_rc" )
+local report = hub.import( "etc_report" )
+local report_activate = cfg.get( "cmd_nickchange_report" )
+local report_hubbot = cfg.get( "cmd_nickchange_report_hubbot" )
+local report_opchat = cfg.get( "cmd_nickchange_report_opchat" )
 
 --// database
 local user_db = "cfg/user.tbl"
+local user_tbl = hub.getregusers()
 local description_file = "scripts/data/cmd_reg_descriptions.tbl"
 
 --// msgs
@@ -127,15 +109,15 @@ local help_desc = lang.help_desc or "change the nickname"
 
 local msg_denied = lang.msg_denied or "You are not allowed to use this command."
 local msg_denied2 = lang.msg_denied2 or "You are not allowed to change the nick of this user."
-local msg_nochange = lang.msg_nochange or "There are no changes needed."
-local msg_nicktaken = lang.msg_nicktaken or "Nick is already taken!"
-local msg_ok = lang.msg_ok or "Nickname was changed to: "
-local msg_disconnect = lang.msg_disconnect or "Nickchange successful, please reconnect with your new nick."
+local msg_nochange = lang.msg_nochange or "[ NICKCHANGE ]--> There are no changes needed."
+local msg_nicktaken = lang.msg_nicktaken or "[ NICKCHANGE ]--> Nick is already taken!"
+local msg_ok = lang.msg_ok or "[ NICKCHANGE ]--> Nickname was changed to: "
+local msg_disconnect = lang.msg_disconnect or "[ NICKCHANGE ]--> Nickchange successful, please reconnect with your new nick."
 local msg_usage = lang.msg_usage or "Usage: [+!#]nickchange mynick <NEW_NICK>  /  [+!#]nickchange othernick <OLD_NICK> <NEW_NICK>"
-local msg_length = lang.msg_length or "Nickname restrictions min/max: %s/%s"
-local msg_op = lang.msg_op or "User %s changed his own nickname to: %s"
-local msg_op2 = lang.msg_op2 or "User %s changed nickname from user: %s  to: %s"
-local msg_notfound = lang.msg_notfound or "Nick not found."
+local msg_length = lang.msg_length or "[ NICKCHANGE ]--> Nickname restrictions min/max: %s/%s"
+local msg_op = lang.msg_op or "[ NICKCHANGE ]--> User %s changed his own nickname to: %s"
+local msg_op2 = lang.msg_op2 or "[ NICKCHANGE ]--> User %s changed nickname from user: %s  to: %s"
+local msg_notfound = lang.msg_notfound or "[ NICKCHANGE ]--> Nick not found."
 
 local ucmd_menu_ct1_0 = lang.ucmd_menu_ct1_0 or { "User", "Control", "Change", "Nickname", "by Nick" }
 local ucmd_menu_ct1_1 = lang.ucmd_menu_ct1_1 or { "About You", "change nickname" }
@@ -157,7 +139,7 @@ local onbmsg, isTaken, isRegged, description_check
 ----------
 
 description_check = function( new_nick, old_nick )
-    local tbl = util_loadtable( description_file )
+    local tbl = util.loadtable( description_file )
     for k, v in pairs( tbl ) do
         if k == old_nick then
             local v1 = v[ "tBy" ]
@@ -168,13 +150,12 @@ description_check = function( new_nick, old_nick )
             tbl[ old_nick ] = nil
         end
     end
-    util_savetable( tbl, "description_tbl", description_file )
+    util.savetable( tbl, "description_tbl", description_file )
 end
 
 --// check if new nick is taken
 isTaken = function( oldnick, newnick )
-    local regusers = hub_getregusers()
-    for i, user in ipairs( regusers ) do
+    for i, user in ipairs( user_tbl ) do
         if user.nick ~= oldnick then
             if user.nick == newnick then
                 return true
@@ -186,8 +167,7 @@ end
 
 --// check if nick is regged
 isRegged = function( nick )
-    local regusers = hub_getregusers()
-    for i, user in ipairs( regusers ) do
+    for i, user in ipairs( user_tbl ) do
         if user.nick == nick then
             return true
         end
@@ -200,42 +180,42 @@ onbmsg = function( user, command, parameters )
     local user_nick = user:nick()
     local user_firstnick = user:firstnick()
     if not user:isregged() then
-        user:reply( msg_denied, hub_getbot )
+        user:reply( msg_denied, hub.getbot() )
         return PROCESSED
     end
     if user_level < minlevel then
-        user:reply( msg_denied, hub_getbot )
+        user:reply( msg_denied, hub.getbot() )
         return PROCESSED
     end
-    local user_tbl = hub_getregusers()
-    local param_1, newnick = utf_match( parameters, "^(%S+)%s(%S+)$" )
-    local param_2, oldnickfrom, newnickfrom = utf_match( parameters, "^(%S+)%s(%S+)%s(%S+)$" )
+    local param_1, newnick = utf.match( parameters, "^(%S+)%s(%S+)$" )
+    local param_2, oldnickfrom, newnickfrom = utf.match( parameters, "^(%S+)%s(%S+)%s(%S+)$" )
 
     if ( param_1 == cmd_param_1 ) and newnick then
         if not nick_change then
-            user:reply( msg_denied, hub_getbot )
+            user:reply( msg_denied, hub.getbot() )
             return PROCESSED
         end
-        if string_len( newnick ) > max_length or string_len( newnick ) < min_length then
-            user:reply( utf_format( msg_length, min_length, max_length ), hub_getbot )
+        if string.len( newnick ) > max_length or string.len( newnick ) < min_length then
+            user:reply( utf.format( msg_length, min_length, max_length ), hub.getbot() )
             return PROCESSED
         end
         if user_firstnick == newnick then
-            user:reply( msg_nochange, hub_getbot )
+            user:reply( msg_nochange, hub.getbot() )
             return PROCESSED
         end
         if isTaken( user_firstnick, newnick ) then
-            user:reply( msg_nicktaken, hub_getbot )
+            user:reply( msg_nicktaken, hub.getbot() )
             return PROCESSED
         else
             for k, v in pairs( user_tbl ) do
                 if user_tbl[ k ].nick == user_firstnick then
                     user_tbl[ k ].nick = newnick
-                    user:reply( msg_ok .. newnick, hub_getbot )
-                    user:kill( "ISTA 230 " .. hub_escapeto( msg_disconnect ) .. "\n", "TL300" )
-                    util_savearray( user_tbl, user_db )
+                    user:reply( msg_ok .. newnick, hub.getbot() )
+                    user:kill( "ISTA 230 " .. hub.escapeto( msg_disconnect ) .. "\n", "TL300" )
+                    util.savearray( user_tbl, user_db )
+                    hub.updateusers()
                     description_check( newnick, user_firstnick )
-                    local msg = utf_format( msg_op, user_firstnick, newnick )
+                    local msg = utf.format( msg_op, user_firstnick, newnick )
                     report.send( report_activate, report_hubbot, report_opchat, oplevel, msg )
                     return PROCESSED
                 end
@@ -243,23 +223,23 @@ onbmsg = function( user, command, parameters )
         end
     elseif ( param_2 == cmd_param_2 ) and newnickfrom then
         if user_level < oplevel then
-            user:reply( msg_denied, hub_getbot )
+            user:reply( msg_denied, hub.getbot() )
             return PROCESSED
         end
         if oldnickfrom == newnickfrom then
-            user:reply( msg_nochange, hub_getbot )
+            user:reply( msg_nochange, hub.getbot() )
             return PROCESSED
         end
-        if string_len( newnickfrom ) > max_length or string_len( newnickfrom ) < min_length then
-            user:reply( utf_format( msg_length, min_length, max_length ), hub_getbot )
+        if string.len( newnickfrom ) > max_length or string.len( newnickfrom ) < min_length then
+            user:reply( utf.format( msg_length, min_length, max_length ), hub.getbot() )
             return PROCESSED
         end
         if not isRegged( oldnickfrom ) then
-            user:reply( msg_notfound, hub_getbot )
+            user:reply( msg_notfound, hub.getbot() )
             return PROCESSED
         end
         if isTaken( oldnickfrom, newnickfrom ) then
-            user:reply( msg_nicktaken, hub_getbot )
+            user:reply( msg_nicktaken, hub.getbot() )
             return PROCESSED
         else
             for k, v in pairs( user_tbl ) do
@@ -267,24 +247,25 @@ onbmsg = function( user, command, parameters )
                     local prefix, target_user
                     local target_level = user_tbl[ k ].level
                     if user_level < target_level then
-                        user:reply( msg_denied2, hub_getbot )
+                        user:reply( msg_denied2, hub.getbot() )
                         return PROCESSED
                     end
                     if activate then
-                        prefix = hub_escapeto( prefix_table[ target_level ] )
-                        target_user = hub_isnickonline( prefix .. oldnickfrom )
+                        prefix = hub.escapeto( prefix_table[ target_level ] )
+                        target_user = hub.isnickonline( prefix .. oldnickfrom )
                     else
-                        target_user = hub_isnickonline( oldnickfrom )
+                        target_user = hub.isnickonline( oldnickfrom )
                     end
                     user_tbl[ k ].nick = newnickfrom
-                    user:reply( msg_ok .. newnickfrom, hub_getbot )
+                    user:reply( msg_ok .. newnickfrom, hub.getbot() )
                     if target_user then
-                        target_user:reply( msg_ok .. newnickfrom, hub_getbot, hub_getbot )
-                        target_user:kill( "ISTA 230 " .. hub_escapeto( msg_disconnect ) .. "\n", "TL300" )
+                        target_user:reply( msg_ok .. newnickfrom, hub.getbot(), hub.getbot() )
+                        target_user:kill( "ISTA 230 " .. hub.escapeto( msg_disconnect ) .. "\n", "TL300" )
                     end
-                    util_savearray( user_tbl, user_db )
+                    util.savearray( user_tbl, user_db )
+                    hub.updateusers()
                     description_check( newnickfrom, oldnickfrom )
-                    local msg = utf_format( msg_op2, user_firstnick, oldnickfrom, newnickfrom )
+                    local msg = utf.format( msg_op2, user_firstnick, oldnickfrom, newnickfrom )
                     report.send( report_activate, report_hubbot, report_opchat, oplevel, msg )
                     return PROCESSED
                 end
@@ -293,10 +274,10 @@ onbmsg = function( user, command, parameters )
     elseif ( param_2 == cmd_param_3 ) and newnickfrom then
         local target_level, target_nick, target_firstnick
         if user_level < oplevel then
-            user:reply( msg_denied, hub_getbot )
+            user:reply( msg_denied, hub.getbot() )
             return PROCESSED
         end
-        for sid, users in pairs( hub_getusers() ) do
+        for sid, users in pairs( hub.getusers() ) do
             if users:nick() == oldnickfrom then
                 target_level = users:level()
                 target_nick = users:nick()
@@ -304,69 +285,70 @@ onbmsg = function( user, command, parameters )
             end
         end
         if target_firstnick == newnickfrom then
-            user:reply( msg_nochange, hub_getbot )
+            user:reply( msg_nochange, hub.getbot() )
             return PROCESSED
         end
-        if string_len( newnickfrom ) > max_length or string_len( newnickfrom ) < min_length then
-            user:reply( utf_format( msg_length, min_length, max_length ), hub_getbot )
+        if string.len( newnickfrom ) > max_length or string.len( newnickfrom ) < min_length then
+            user:reply( utf.format( msg_length, min_length, max_length ), hub.getbot() )
             return PROCESSED
         end
         if isTaken( target_firstnick, newnickfrom ) then
-            user:reply( msg_nicktaken, hub_getbot )
+            user:reply( msg_nicktaken, hub.getbot() )
             return PROCESSED
         else
             if user_level < target_level then
-                user:reply( msg_denied2, hub_getbot )
+                user:reply( msg_denied2, hub.getbot() )
                 return PROCESSED
             end
-            local target = hub_isnickonline( target_nick )
+            local target = hub.isnickonline( target_nick )
             for k, v in pairs( user_tbl ) do
                 if user_tbl[ k ].nick == target_firstnick then
                     user_tbl[ k ].nick = newnickfrom
-                    user:reply( msg_ok .. newnickfrom, hub_getbot )
-                    target:reply( msg_ok .. newnickfrom, hub_getbot, hub_getbot )
-                    target:kill( "ISTA 230 " .. hub_escapeto( msg_disconnect ) .. "\n", "TL300" )
-                    util_savearray( user_tbl, user_db )
+                    user:reply( msg_ok .. newnickfrom, hub.getbot() )
+                    target:reply( msg_ok .. newnickfrom, hub.getbot(), hub.getbot() )
+                    target:kill( "ISTA 230 " .. hub.escapeto( msg_disconnect ) .. "\n", "TL300" )
+                    util.savearray( user_tbl, user_db )
+                    hub.updateusers()
                     description_check( newnickfrom, target_firstnick )
-                    local msg = utf_format( msg_op2, user_firstnick, target_firstnick, newnickfrom )
+                    local msg = utf.format( msg_op2, user_firstnick, target_firstnick, newnickfrom )
                     report.send( report_activate, report_hubbot, report_opchat, oplevel, msg )
                     return PROCESSED
                 end
             end
         end
     else
-        user:reply( msg_usage, hub_getbot )
+        user:reply( msg_usage, hub.getbot() )
         return PROCESSED
     end
 end
 
 hub.setlistener( "onStart", {},
     function()
-        help = hub_import( "cmd_help" )
+        help = hub.import( "cmd_help" )
         if help then help.reg( help_title, help_usage, help_desc, minlevel ) end
-        ucmd = hub_import( "etc_usercommands" )
+        ucmd = hub.import( "etc_usercommands" )
         if ucmd then
             ucmd.add( ucmd_menu_ct1_1, cmd, { cmd_param_1, "%[line:" .. ucmd_popup .. "]" }, { "CT1" }, minlevel )
             ucmd.add( ucmd_menu_ct1_0, cmd, { cmd_param_2, "%[line:" .. ucmd_popup2 .. "]", "%[line:" .. ucmd_popup .. "]" }, { "CT1" }, oplevel )
             if advanced_rc then
-                local regusers, reggednicks, reggedcids = hub_getregusers()
+                local user_tbl = hub.getregusers()
                 local usertbl = {}
-                for i, user in ipairs( regusers ) do
+                for i, user in ipairs( user_tbl ) do
                     if ( user.is_bot ~=1 ) and user.nick then
-                      table_insert( usertbl, user.nick )
+                      table.insert( usertbl, user.nick )
                     end
                 end
-                table_sort( usertbl )
+                table.sort( usertbl )
                 for _, nick in pairs( usertbl ) do
                     ucmd.add( { ucmd_menu_ct1_2, ucmd_menu_ct1_3, ucmd_menu_ct1_4, ucmd_menu_ct1_5, ucmd_menu_ct1_6, nick }, cmd, { cmd_param_2, nick, "%[line:" .. ucmd_popup .. "]" }, { "CT1" }, oplevel )
                 end
             end
             ucmd.add( ucmd_menu_ct2_1, cmd, { cmd_param_3, "%[userNI]", "%[line:" .. ucmd_popup .. "]" }, { "CT2" }, oplevel )
         end
-        hubcmd = hub_import( "etc_hubcommands" )
+        hubcmd = hub.import( "etc_hubcommands" )
         assert( hubcmd )
         assert( hubcmd.add( cmd, onbmsg ) )
     end
 )
 
-hub_debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " **" )
+hub.debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " **" )
