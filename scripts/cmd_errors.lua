78fb01dcf766e2528c3b24442b2bc04e588d69cf
pulsar-de <pulsar@pulsnet.org>
2021-10-16 21:31:29 +0200

    - update: "scripts/cmd_errors.lua"     - removed table lookups     - removed unused code     - changed visuals

--- scripts/cmd_errors.lua
+++ scripts/cmd_errors.lua
@@ -5,6 +5,11 @@
         - this script adds a command "errors" to get hub errors, it also feeds errors to hubowners
         - usage: [+!#]errors
 
+        v0.12: by pulsar
+            - removed table lookups
+            - removed unused code
+            - changed visuals
+
         v0.11: by pulsar
             - added "onError" listener to feed errors to hubowners
             - added maxlines limit to send
@@ -47,34 +52,19 @@
 --------------
 
 local scriptname = "cmd_errors"
-local scriptversion = "0.11"
+local scriptversion = "0.12"
 
 local cmd = "errors"
 
 local maxlines = 200
 
-
-----------------------------
---[DEFINITION/DECLARATION]--
-----------------------------
-
---// table lookups
-local cfg_get = cfg.get
-local cfg_loadlanguage = cfg.loadlanguage
-local hub_getbot = hub.getbot()
-local hub_import = hub.import
-local hub_debug = hub.debug
-local util_getlowestlevel = util.getlowestlevel
-local table_concat = table.concat
-local io_open = io.open
-
 --// imports
-local scriptlang = cfg_get( "language" )
-local permission = cfg_get( "cmd_errors_permission" )
+local scriptlang = cfg.get( "language" )
+local permission = cfg.get( "cmd_errors_permission" )
 local path = "log/error.log"
 
 --// msgs
-local lang, err = cfg_loadlanguage( scriptlang, scriptname ); lang = lang or {}; err = err and hub_debug( err )
+local lang, err = cfg.loadlanguage( scriptlang, scriptname ); lang = lang or {}; err = err and hub.debug( err )
 
 local help_title = lang.help_title or "cmd_errors.lua"
 local help_usage = lang.help_usage or "[+!#]errors"
@@ -82,40 +72,40 @@ local help_desc = lang.help_desc or "Sends error.log"
 
 local ucmd_menu = lang.ucmd_menu or { "Hub", "Logs", "show", "error.log" }
 
-local msg_denied = lang.msg_denied or "You are not allowed to use this command."
-local msg_noerrors = lang.msg_noerrors or "No errors"
+local msg_denied = lang.msg_denied or "[ ERRORS ]--> You are not allowed to use this command."
+local msg_noerrors = lang.msg_noerrors or "[ ERRORS ]--> No errors."
 
 
 ----------
 --[CODE]--
 ----------
 
-local minlevel = util_getlowestlevel( permission )
+local minlevel = util.getlowestlevel( permission )
 local report_send
 
 local onbmsg = function( user, command, parameters )
     if not permission[ user:level() ] then
-        user:reply( msg_denied, hub_getbot )
+        user:reply( msg_denied, hub.getbot() )
         return PROCESSED
     end
     local tbl = {}
-    local file, err = io_open( path, "r" )
+    local file, err = io.open( path, "r" )
     if file then
         for line in file:lines() do tbl[ #tbl + 1 ] = line end
         file:close()
     end
     if next( tbl ) == nil then
-        user:reply( msg_noerrors, hub_getbot )
+        user:reply( msg_noerrors, hub.getbot() )
     else
         if #tbl < maxlines then
-            user:reply( "\n\n" .. table_concat( tbl, "\n" ) .. "\n", hub_getbot, hub_getbot )
+            user:reply( "\n\n" .. table.concat( tbl, "\n" ) .. "\n", hub.getbot(), hub.getbot() )
         else
             local s, e, msg = 1, #tbl - maxlines, ""
             for k, v in ipairs( tbl ) do
                 if s >= e then msg = msg .. v .. "\n" end
                 s = s + 1
             end
-            user:reply( "\n\n" .. msg .. "\n", hub_getbot, hub_getbot )
+            user:reply( "\n\n" .. msg .. "\n", hub.getbot(), hub.getbot() )
         end
     end
     return PROCESSED
@@ -123,29 +113,28 @@ end
 
 hub.setlistener( "onError", { },  -- when this function produces any error, it wont be reported to avoid endless loops
     function( msg )
-        --report_send( msg, 100, 100, hub_getbot, hub_getbot )  -- old method
         report_send( true, true, false, 100, msg )  -- new method
     end
 )
 
 hub.setlistener( "onStart", { },
     function( )
-        local help = hub_import( "cmd_help" )
+        local help = hub.import( "cmd_help" )
         if help then
             help.reg( help_title, help_usage, help_desc, minlevel )
         end
-        local ucmd = hub_import( "etc_usercommands" )
+        local ucmd = hub.import( "etc_usercommands" )
         if ucmd then
             ucmd.add( ucmd_menu, cmd, { }, { "CT1" }, minlevel )
         end
-        local hubcmd = hub_import( "etc_hubcommands" )
+        local hubcmd = hub.import( "etc_hubcommands" )
         assert( hubcmd )
         assert( hubcmd.add( cmd, onbmsg ) )
-        local report = hub_import( "etc_report" )
+        local report = hub.import( "etc_report" )
         assert( report )
         report_send = report.send
         return nil
     end
 )
 
-hub_debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " **" )
+hub.debug( "** Loaded " .. scriptname .. " " .. scriptversion .. " **" )
