9fd42b8d807032fafb7d2e0f0a4a2f7c9826b3e2
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2022-01-27 19:42:53 +0100

    Remove trailing whitespace in make_keyprint.sh

--- examples/certs/make_keyprint.sh
+++ examples/certs/make_keyprint.sh
@@ -7,7 +7,7 @@
 #
 #
 # --// english description //--
-# 
+#
 # This bash script generates a keyprint of a particular certificate in Linux, as used in the KEYP extension.
 # The script uses OpenSSL, filters all the verbosity OpenSSL adds and then uses Python to encode it into Base32.
 # The script first calls OpenSSL to get the fingerprint then remove the verbosity and the colons convert with Python and remove any padding.
