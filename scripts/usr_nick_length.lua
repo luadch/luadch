853ce1a1d6ce1b5df153536c57d158cf3607ee31
blasti <blasti@users.noreply.github.com>
2022-03-09 21:55:03 +0100

    Fix typos

--- scripts/usr_nick_length.lua
+++ scripts/usr_nick_length.lua
@@ -2,7 +2,7 @@
 
     usr_nick_length.lua by blastbeat
 
-        - this script checks for proper nicknames on connect
+        - this script checks for proper nicknames onConnect and onInf
 
 ]]--
 
@@ -11,15 +11,15 @@ local scriptname = "usr_nick_length.lua"
 local scriptversion = "0.01"
 
 local check = function( user, nick )
-        local len = #nick -- todo: doesn't consider utf8 codepoints
-        if ( cfg.get "min_nickname_length" <= len ) and ( len <= cfg.get "max_nickname_length" ) then
-            return nil
-        end
-        --remember: never fire listenter X inside listener X; will cause infinite loop
-        scripts.firelistener( "onFailedAuth", nick, user:ip( ), user:cid( ), "Invalid nick length: " .. len ) -- todo: i18n
-        user:kill( "ISTA 221 " .. hub.escapeto( "Invalid nick length." ) .. "\n", "TL300" )
-        return PROCESSED
+    local len = #nick -- todo: doesn't consider utf8 codepoints
+    if ( cfg.get "min_nickname_length" <= len ) and ( len <= cfg.get "max_nickname_length" ) then
+        return nil
     end
+    --remember: never fire listenter X inside listener X; will cause infinite loop
+    scripts.firelistener( "onFailedAuth", nick, user:ip( ), user:cid( ), "Invalid nick length: " .. len ) -- todo: i18n
+    user:kill( "ISTA 221 " .. hub.escapeto( "Invalid nick length." ) .. "\n", "TL300" )
+    return PROCESSED
+end
 
 hub.setlistener( "onConnect", { },
     function( user )
