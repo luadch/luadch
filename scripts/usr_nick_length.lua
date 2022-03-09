fd1d0b198d66ad3815b1c9bb38928e38290ccd63
blasti <blasti@users.noreply.github.com>
2022-03-09 21:52:28 +0100

    Add onInf listener to nick_length script

--- scripts/usr_nick_length.lua
+++ scripts/usr_nick_length.lua
@@ -10,19 +10,30 @@
 local scriptname = "usr_nick_length.lua"
 local scriptversion = "0.01"
 
-cfg.get( "cmd_ban_report_opchat" )
-
--- add prefix to connecting user
-hub.setlistener( "onConnect", { },
-    function( user )
-        local len = #user:nick() -- todo: doesn't consider utf8 codepoints
-        if ( cfg.get "min_nickname_length" <= len ) and ( len <= cfg.get " max_nickname_length" ) then
+local check = function( user, nick )
+        local len = #nick -- todo: doesn't consider utf8 codepoints
+        if ( cfg.get "min_nickname_length" <= len ) and ( len <= cfg.get "max_nickname_length" ) then
             return nil
         end
         --remember: never fire listenter X inside listener X; will cause infinite loop
-        scripts.firelistener( "onFailedAuth", user:nick( ), user:ip( ), user:cid( ), "Invalid nick length: " .. len ) -- todo: i18n
+        scripts.firelistener( "onFailedAuth", nick, user:ip( ), user:cid( ), "Invalid nick length: " .. len ) -- todo: i18n
         user:kill( "ISTA 221 " .. hub.escapeto( "Invalid nick length." ) .. "\n", "TL300" )
         return PROCESSED
+    end
+
+hub.setlistener( "onConnect", { },
+    function( user )
+        return check( user, user:nick( ) )
+    end
+)
+
+hub.setlistener( "onInf", { },
+    function( user, cmd )
+        for name, value in cmd:getallnp( ) do
+            if name == "NI" then
+                return check( user, value )
+            end
+        end
     end
 )
 
