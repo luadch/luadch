6ded66217a2a46e5f6f81643ea2b805e7b06d572
pulsar-de <pulsar@pulsnet.org>
2022-04-02 09:01:55 +0200

    cfg.tbl: changed some presets and comments

--- core/const.lua
+++ core/const.lua
@@ -11,7 +11,7 @@
 return {
 
     PROGRAM_NAME = "Luadch",
-    VERSION = "v2.23 RC10",
+    VERSION = "v2.23",
     COPYRIGHT = "by blastbeat and pulsar",
     CONFIG_PATH = "././cfg/",
     -- LOG_PATH = "././log/",
